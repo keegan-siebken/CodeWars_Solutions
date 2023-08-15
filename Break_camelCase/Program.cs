785acac4c2e10ad4491ca055011ad544a2230eee
Keegan Siebken <keegan.siebken@gmail.com>
2023-08-15 13:58:54 +0100

    Complete initial solution, tests passed

---
+++ Break_camelCase/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Challenge information:
+
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Examples:
+
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+*/
+
+//set up test example
+
+using System.Text.RegularExpressions;
+
+string str = "camelCasingTestAgain";
+
+//Try direct approach with RegEx:
+
+var result = Regex.Replace(str, @"(\p{Lu})", " $1");
+
+/*
+regEx explanation:
+
+\p{L} is a Unicode property shorthand for \p{Letter}, matching any letter from any language. \p{Lu} matches an uppercase letter with a lowercase variant,
+and \p{Ll} will match a lowercase letter that has an uppercase variant.
+
+$1 is the captured capital letter, so having the replacement string be " $1" replaces the capital letter with a space and then the capital letter.
+*/
+
+Console.WriteLine(result);
